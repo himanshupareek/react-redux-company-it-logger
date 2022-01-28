33dba9b63a504ff930fc58393b60d4032f1c84fb
Himanshu Pareek <himparee@publicisgroupe.net>
2022-01-28 22:52:51 +0530

    commit the code - company-it-logger

---
+++ README.md
@@ -0,0 +1,18 @@
+# company-it-logger
+
+> React app to track company's IT department tasks and issues. Uses JSON-Server as a mock backend.
+
+## Usage
+
+### `npm install`
+
+### `npm run dev`
+
+Runs the app with the backend JSON-Server<br>
+Open [http://localhost:3000](http://localhost:3000)
+Server [http://localhost:5020](http://localhost:5020)
+db.json contains data
+
+### `npm run build`
+
+Builds the app for production to the `build` folder.<br>
---
+++ src/components/techs/TechItem.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import M from 'materialize-css/dist/js/materialize.min.js';
+
+import { deleteTech } from '../../actions/techActions';
+
+const TechItem = ( { deleteTech, tech: { id, firstName, lastName} }) => {
+  const onDelete = () => {
+    deleteTech(id);
+    M.toast({ html: 'Technician deleted' });
+  };
+
+  return (
+    <li className='collection-item'>
+      <div>
+        {firstName} {lastName}
+        <a href='#!' className='secondary-content' onClick={onDelete}>
+          <i className='material-icons grey-text'>delete</i>
+        </a>
+      </div>
+    </li>
+  );
+};
+
+TechItem.propTypes = {
+  tech: PropTypes.object.isRequired,
+  deleteTech: PropTypes.func.isRequired,
+}
+
+export default connect(null, { deleteTech })(TechItem);
